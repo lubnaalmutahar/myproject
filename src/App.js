826bf18eaa6f9cbb75ab09490bae41af7031c79a
Lubna Almutahar <lubna_almutahar@yahoo.com>
2022-07-04 23:35:51 +0800

    secondt commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './screens/home.js';
+import Features from './screens/features.js';
+import Contact from './screens/contact.js';
+import Signup from './screens/signup.js';
+import Login from './screens/login.js';
+import EditAccount from './screens/editAccount.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className="App">
+      <Home />
+      <Features />
+      <Contact /> 
+      <Signup />
+      <Login />
+      <EditAccount />
+    </div>      
   );
 }
 
